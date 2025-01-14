677b11d0da78bf8476c5e16cbbd9840f3dcefe2b
Def <sofiasittykova@gmail.com>
2025-01-14 11:31:17 +0500

    update

--- TEST_FITSFILE_3/monitor2.bash
+++ TEST_FITSFILE_3/monitor2.bash
@@ -4,6 +4,13 @@ LOCKFILE="/tmp/process.lock"
 TMPDIR="TMP"
 LOGFILE="${TMPDIR}/accuracy_log.txt"
 
+# Очистка TMP перед началом работы
+clear_tmp_dir() {
+    echo "cleaning ${TMPDIR}..."
+    rm -rf ${TMPDIR}/*
+    echo "Done."
+}
+
 process_file() {
     NEWFILE="$1"
     echo "${NEWFILE} uploaded"
@@ -17,7 +24,7 @@ process_file() {
     
 
     sleep 1
-    echo '===========  Sources extractions ======================='
+    # echo '===========  Sources extractions ======================='
     # sex "${NEWFILE}" -c CONFIGS/default.sex
     # sex "${NEWFILE}" -c CONFIGS/defaultTEST.sex
 
@@ -28,7 +35,7 @@ process_file() {
     python SRC/WFT_19072024.py "${NEWFILE}"
 
     echo '===========  astrometry up TMP directory  =============='
-    python SRC/xy2radec.py "${NEWFILE}"
+    # python SRC/xy2radec.py "${NEWFILE}"
 
     echo '===========  astrometry up TMP directory  =============='
     python radec_without_mode.py
@@ -38,64 +45,24 @@ process_file() {
         exit 1
     fi
 
-    # rm -rf ${TMPDIR}/*
-
     echo "${NEWFILE}" > ${TMPDIR}/processing_log.txt
 
     rm -f "${NEWFILE}"
     : > ${TMPDIR}/processing_log.txt
 
-
-    # echo "Corect: ${NEWFILE}:"
-    # read correct
-    # echo "Incorrect: ${NEWFILE}:"
-    # read incorrect
-
-    # echo "Правильный объект был обнаружен в ${NEWFILE} (y/n)?"
-    # read found_correct_object
-    # if [ "$found_correct_object" == "y" ]; then
-    #     correct_files=1
-    # else
-    #     correct_files=0
-    # fi
-
-    # echo "${NEWFILE},Correct:${correct},Incorrect:${incorrect},CorrectFile:${correct_files}" >> ${LOGFILE}
-
-    echo '==================  APEX  =============================='
-    # source ~/anaconda3/etc/profile.d/conda.sh
-    # conda activate apex_env
-    # echo "current conda env: $(conda info --envs | grep \* | awk '{print $1}')"
-    # python GSS_Apex_TURBO.py
-    # conda deactivate
-
-
     echo '==================  DONE  =============================='
 }
 
-# # Запуск обработки файлов последовательно
-# while true; do
-#     for NEWFILE in "${MONITORDIR}"*; do
-#         if [ -f "$NEWFILE" ]; then
-#             {
-#                 # Ожидаем, пока не получим блокировку
-#                 exec 200>$LOCKFILE
-#                 flock -x 200
-#                 process_file "${NEWFILE}"
-#             }
-#         fi
-#     done
+# Очистка TMP перед запуском мониторинга
+clear_tmp_dir
 
-#     # Добавляем задержку перед повторной проверкой
-#     sleep 1
-# done
-
-# Запускаем обработку файлов с блокировкой
 inotifywait -m -e create --format '%w%f' "${MONITORDIR}" | while read NEWFILE
 do
     {
-        # Ожидаем, пока не получим блокировку
         exec 200>$LOCKFILE
         flock -x 200
         process_file "${NEWFILE}"
+        # Очистка TMP после обработки каждого файла (если нужно)
+        clear_tmp_dir
     }
 done
